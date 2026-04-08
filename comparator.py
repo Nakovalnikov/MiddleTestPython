8c0e6fb5ee149943c3644639d8be830fb0fe98bc
Timur <LetTimMinekraft@gmail.com>
2026-04-08 18:54:06 +0300

    PEP 8 comparator

--- comparator.py
+++ comparator.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def get_file_content(filepath):
     try:
         with open(filepath, 'r', encoding='utf-8') as f:
@@ -7,23 +8,25 @@ def get_file_content(filepath):
     except FileNotFoundError:
         return set()
 
+
 def compare_lines(set1, set2):
     same = sorted(list(set1.intersection(set2)))
     diff = sorted(list(set1.symmetric_difference(set2)))
-    return same, diff        
+    return same, diff
+
 
 def save_to_file(lines, filename):
     with open(filename, 'w', encoding='utf-8') as f:
         f.write('\n'.join(lines))
 
+
 def main(file1, file2):
     content1 = get_file_content(file1)
     content2 = get_file_content(file2)
-
     same, diff = compare_lines(content1, content2)
-
     save_to_file(same, "same.txt")
     save_to_file(diff, "diff.txt")
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
