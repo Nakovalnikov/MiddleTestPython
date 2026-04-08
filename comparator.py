bef85a73c5cd365c5a8b41473432d64eeffa6026
Timur <LetTimMinekraft@gmail.com>
2026-04-08 18:32:29 +0300

    add save to file funk

--- comparator.py
+++ comparator.py
@@ -12,9 +12,18 @@ def compare_lines(set1, set2):
     diff = sorted(list(set1.symmetric_difference(set2)))
     return same, diff        
 
+def save_to_file(lines, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(lines))
+
 def main(file1, file2):
     content1 = get_file_content(file1)
     content2 = get_file_content(file2)
+
+    same, diff = compare_lines(content1, content2)
+
+    save_to_file(same, "same.txt")
+    save_to_file(diff, "diff.txt")
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
